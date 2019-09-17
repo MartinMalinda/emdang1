35e6c44c4368f148f49282237370afaff63dc06a
dangm <dang.m@seznam.cz>
2019-09-17 17:45:20 +0200

    update

--- week-02/day-01/ConsoleApp2/ConsoleApp2/Program.cs
+++ week-02/day-01/ConsoleApp2/ConsoleApp2/Program.cs
@@ -10,7 +10,22 @@ namespace ConsoleApp2
     {
         static void Main(string[] args)
         {
-            // Write a function called `sum` that returns the sum of numbers from zero to the given parameter
+            //  Create a function that takes a number and an array of integers as a parameter
+            //  Returns the indices of the integers in the array of which the first number is a part of
+            //  Or returns an empty array if the number is not part of any of the integers in the array
+
+            //  Example:
+            // Console.WriteLine(SubInt(1, new int[] { 1, 11, 34, 52, 61 }));
+            //  should print: `[0, 1, 4]`
+            // Console.WriteLine(SubInt(9, new int[] { 1, 11, 34, 52, 61 }));
+            //  should print: '[]'
+
+
+            for (int i = 0; i < length; i++)
+            {
+
+            }
+
 
             Console.Write("Please write the end-number: ");
             int endNumber = int.Parse(Console.ReadLine());
