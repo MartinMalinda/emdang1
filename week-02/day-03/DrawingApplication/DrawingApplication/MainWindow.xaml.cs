547a8e35d91f67a168def8d329059365e31c4142
dangm <dang.m@seznam.cz>
2019-09-19 23:17:49 +0200

    drawing update

--- week-02/day-03/DrawingApplication/DrawingApplication/MainWindow.xaml.cs
+++ week-02/day-03/DrawingApplication/DrawingApplication/MainWindow.xaml.cs
@@ -20,13 +20,27 @@ namespace DrawingApplication
 
             double canvasWidth = canvas.Width = 600;           //zadefinování canvasu, protože to je jinak nekonečno
             double canvasHeight = canvas.Height = 600;         //zadefinování canvasu, protože to je jinak nekonečno
-            //foxDraw.SetBackgroundColor(Colors.Black);
+                                                               //foxDraw.SetBackgroundColor(Colors.Black);
 
             /*----------------------------------------------------------------------------------------------------------*/
-            //  PURPLE STEPS 3D
+            // Create a function that draws a single line and takes 3 parameters:
+            // the x and y coordinates of the line's starting point and the
+            // foxDraw and draws a line from that point to the center of the
+            // canvas.
+            // Fill the canvas with lines from the edges, every 20 px, to the center.
 
-            PurpleSteps(foxDraw, 5, 20);
-           
+            for (int i = 0; i < canvasWidth; i++)
+            {
+                for (int i = 0; i <= canvasHeight; i++)
+                {
+                    GoToCenter(foxDraw, i * 20, j * 20);         
+                }
+            }
+            
+
+
+
+
         }
 
         private void InitializeComponent()
@@ -35,17 +49,13 @@ namespace DrawingApplication
         }
 
 
-        public void PurpleSteps(FoxDraw foxDraw, int mnozstvi, double x)  // i = kolikrát, x = size
+        public void GoToCenter(FoxDraw foxDraw, int x, int y)
         {
-            for (int i = 1; i <= mnozstvi; i++)
-            {
-                foxDraw.SetFillColor(Colors.Purple);
-                foxDraw.DrawRectangle((x*i) + (2 * i), x*i + (2 * i), x+(2*i), x + (2 * i));
-
-            }
-            
+            foxDraw.SetStrokeColor(Colors.Black);
+            foxDraw.DrawLine(x, y, Height / 2, Width / 2);
 
         }
+
 
 
     }
