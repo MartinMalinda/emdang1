9cf3b2d4b85102328a1fa625335b56dac3bc5f8d
dangm <dang.m@seznam.cz>
2019-10-03 17:42:53 +0200

    updated testing exes

---
+++ week-04/day-04/CountLetters/CountLetters/Letters.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CountLetters
+{
+    class Letters
+    {
+        public Letters()
+        {
+
+        }
+
+        public void CountLettersMethod(string word)
+        {
+            //// declaring a dictionary for the result
+            //Dictionary<char, int> dictionary = new Dictionary<char, int>();
+
+            //// transforming string into charArray
+            //char[] charArray = word.ToCharArray();
+
+            //// list for storing occurences
+            //List<int> occurList = new List<int>();
+
+            //// counter for occurencies
+            //int counter = 1;
+
+            //for (int i = 0; i < charArray.Length; i++)
+            //{
+            //    for (int j = i; j < charArray.Length; j++)
+            //    {
+            //        if (charArray[j] == charArray[i])
+            //        {
+            //            counter++;
+            //        }
+            //        //dictionary.Add()
+            //        occurList.Add(counter);
+            //    }
+            //}
+
+            //foreach (var item in occurList)
+            //{
+            //    Console.WriteLine(item);
+            //}
+
+            
+            char[] charArray = word.ToLower().ToCharArray();
+            Dictionary<char, int> dictionary = new Dictionary<char, int>();
+            //int tempVar = 0;
+            foreach (var item in charArray)
+            {
+                if (dictionary.ContainsKey(item))
+                {
+                    //dictionary[item] += 1;
+                    dictionary[item]++;
+                }
+                else
+                {
+                    dictionary.Add(item, 1);
+                }
+            }
+
+            foreach (var item in dictionary)
+            {
+                Console.WriteLine(string.Format(item.Key + ": " + item.Value + " times"));
+            }
+
+
+        }
+    }
+}
---
+++ week-04/day-04/CountLetters/CountLetters/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CountLetters
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //string test = "Lalalo";
+            string test = "La la lo";
+            Letters letter = new Letters();
+
+            letter.CountLettersMethod(test);
+
+            Console.ReadLine();
+        }
+    }
+}
