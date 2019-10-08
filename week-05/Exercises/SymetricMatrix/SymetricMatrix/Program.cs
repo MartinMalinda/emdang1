5c43638f12e9593436acf5ea8251a69292c7281c
dangm <dang.m@seznam.cz>
2019-10-08 23:43:56 +0200

    added symetric matrix exe

---
+++ week-05/Exercises/SymetricMatrix/SymetricMatrix/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SymetricMatrix
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int[,] matrix = new int[, ] { { 1, 0, 1 },
+                                          { 0, 2, 2 },
+                                          { 1, 2, 5 }
+            };
+
+            Console.WriteLine(IsSymetric(matrix));
+            Console.ReadLine();
+        }
+
+        public static bool IsSymetric(int[,] inputMatrix)
+        {
+          
+            for (int i = 0; i < inputMatrix.GetLength(0)-1; i++)
+            {
+                if (inputMatrix[i, (i + 1)] != inputMatrix[(i + 1), i])
+                {
+                    return false;
+                }
+            }
+            return true;
+        }
+    }
+
+}
