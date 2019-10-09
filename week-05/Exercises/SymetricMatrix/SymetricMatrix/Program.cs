ee3a8251ec263e8ace5ea97edcfc8ee37bc099a3
dangm <dang.m@seznam.cz>
2019-10-09 09:51:34 +0200

    finalized symetricMatrix exe, fixed loop logic

--- week-05/Exercises/SymetricMatrix/SymetricMatrix/Program.cs
+++ week-05/Exercises/SymetricMatrix/SymetricMatrix/Program.cs
@@ -16,16 +16,22 @@ namespace SymetricMatrix
                                           { 1, 2, 5 }
             };
 
-            Console.WriteLine(IsSymetric(matrix));
+            int[,] matrix2 = new int[,] { { 1, 0, 1, 2 },
+                                          { 0, 2, 2, 3 },
+                                          { 1, 2, 5, 4 },
+                                          { 2, 2, 5, 3 }
+            };
+
+            Console.WriteLine(IsSymetric(matrix2));
             Console.ReadLine();
         }
 
         public static bool IsSymetric(int[,] inputMatrix)
         {
           
-            for (int i = 0; i < inputMatrix.GetLength(0)-1; i++)        // -1 due to the index bounds
+            for (int i = 1; i < inputMatrix.GetLength(0); i++)        // -1 due to the index bounds
             {
-                if (inputMatrix[i, (i + 1)] != inputMatrix[(i + 1), i])
+                if (inputMatrix[0, i] != inputMatrix[i, 0])
                 {
                     return false;
                 }
