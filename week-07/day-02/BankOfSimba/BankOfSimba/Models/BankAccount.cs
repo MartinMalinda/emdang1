b843e9e1b4c40466d7fc06d8f3e671c50c0abd41
dangm <dang.m@seznam.cz>
2019-10-29 13:07:57 +0100

    added BankOfSimba project + first exes

---
+++ week-07/day-02/BankOfSimba/BankOfSimba/Models/BankAccount.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BankOfSimba.Models
+{
+    public class BankAccount
+    {
+        public string Name { get; set; }
+        public double Balance { get; set; }
+        public string AnimalType { get; set; }
+        public string Currency { get; set; } = "Zebra";
+
+        public BankAccount(string name, double balance, string animalType)
+        {
+            Name = name;
+            Balance = balance;
+            AnimalType = animalType;
+        }
+    }
+}
