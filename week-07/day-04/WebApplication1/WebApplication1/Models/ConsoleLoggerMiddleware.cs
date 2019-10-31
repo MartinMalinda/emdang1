000dad8fc47cd0601a8903f4f41dc05cacc3073c
dangm <dang.m@seznam.cz>
2019-10-31 11:15:32 +0100

    added dependencyInjection project

---
+++ week-07/day-04/WebApplication1/WebApplication1/Models/ConsoleLoggerMiddleware.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Http;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApplication1.Models
+{
+    public class ConsoleLoggerMiddleware : IMiddleware
+    {
+        private Printer printer;
+
+        public ConsoleLoggerMiddleware(Printer printer)
+        {
+            this.printer = printer;
+        }
+
+        public Task InvokeAsync(HttpContext context, RequestDelegate next) // ????????????????????????????????????????
+        {
+            Console.WriteLine("Hi from the middleware");
+            printer.Log("HELLOOOOO");
+
+            return next(context);
+        }
+    }
+}
